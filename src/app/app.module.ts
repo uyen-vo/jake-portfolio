61210cb1b5e2d5a8451550e8806e58063058a625
uyenvo <uyend.vo@gmail.com>
2020-10-23 00:12:48 -0500

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectDetailComponent,
+    ProjectItemComponent
   ],
   imports: [
     BrowserModule,
